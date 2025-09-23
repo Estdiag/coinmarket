4030bb075bb5699801666b3942a7d212e0256f1c
Estdiag <estefanydiaz75@gmail.com>
2025-09-23 09:52:32 -0500

    refactor: enhance server initialization and route management

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,24 +1,40 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import { sequelize } from './models';
+import { sequelize } from './models'; 
+import { defineAssociations } from './models/associations'; 
+import cryptocurrencyRoutes from './routes/cryptocurrency.routes';
+import priceHistoryRoutes from './routes/pricehistory.routes';
+import { initSchedulers } from './schedulers/syncData.schedulers';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-
 app.use(express.json());
+
+// Rutas
+app.use('/api', cryptocurrencyRoutes);
+app.use('/api', priceHistoryRoutes);
+
 
 const startServer = async (): Promise<void> => {
   try {
+      defineAssociations();
+    console.log('✅ Asociaciones definidas');
     
-    await sequelize.sync();
-    console.log('✅ Base de datos sincronizada');
 
-    app.listen(PORT, () => {
-      console.log(`🚀 Servidor ejecutándose en puerto ${PORT}`);
+    await sequelize.sync({ 
+      force: true, 
+      // alter: process.env.NODE_ENV === 'development' 
     });
+    console.log('✅ Base de datos sincronizada');
+    
+   
+    app.listen(PORT, () => {
+      console.log(`🚀 Servidor ejecutándose en puerto ${PORT}`);      
+    });
+    initSchedulers()
   } catch (error) {
     console.error('❌ Error iniciando servidor:', error);
     process.exit(1);
